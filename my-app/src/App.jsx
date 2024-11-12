c3efb2473ca9775a3166f3026e323ed568475546
ArabDeveloper <arabdevelopers731@gmail.com>
2024-11-12 13:16:22 +0200

    7.react class component example and basic syntax

---
+++ my-app/src/App.jsx
@@ -0,0 +1,17 @@
+// define component
+
+// 1.class component 
+import React from 'react'
+
+class HellWorld extends React.Component {
+
+    // concate 2 chars 
+
+    render() {
+        return (
+            <h1>Hello World </h1>
+        )
+    }
+}
+
+export default HellWorld
---
+++ my-app/src/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client'
+import HellWorld from './App.jsx'
+
+
+createRoot(document.getElementById('root')).render(<HellWorld />)
