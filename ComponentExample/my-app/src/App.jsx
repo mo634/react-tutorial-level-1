7229f03bcdaa9f62e648d35d41eb8272584aba08
ArabDeveloper <arabdevelopers731@gmail.com>
2024-11-26 09:28:40 +0200

    13.props and states example part 3 : passing default props

--- ComponentExample/my-app/src/App.jsx
+++ ComponentExample/my-app/src/App.jsx
@@ -9,18 +9,31 @@ export default class App extends Component {
                     title="Admin Dashboard"
                     desc="This is Admin Dashboard Website"
                     tech="HTMl,CSS,JS"
+                    numbeOfDays={10}
                 />
+
                 <ProjectSection
                     title="youtube clone"
                     desc="This is youtube clone"
                     tech="HTMl,CSS,JS,React js"
+                    numbeOfDays={5}
+
                 />
+
                 <ProjectSection
                     title="tiktok clone"
                     desc="This is tiktok clone"
                     tech="HTMl,CSS,Tailwind,Nextjs"
-
+                    numbeOfDays={4}
                 />
+
+                <ProjectSection
+                    title="facebook clone"
+                    desc="This is facebook clone"
+                    tech="HTMl,CSS,Tailwind,Nextjs,mongodb"
+                />
+
+
             </div>
         )
     }
--- ComponentExample/my-app/src/components/ProjectSection.jsx
+++ ComponentExample/my-app/src/components/ProjectSection.jsx
@@ -1,18 +1,32 @@
 import React, { Component } from 'react'
 
 export default class ProjectSection extends Component {
+
+    static defaultProps = {
+        numbeOfDays: 0
+    }
     render() {
         return (
-            <section>
-                <p>{this.props.title}</p>
+            <>
+                <section>
+                    <p>{this.props.title}</p>
 
-                <p>{this.props.desc} </p>
+                    <p>{this.props.desc} </p>
 
-                <div className="">
-                    {this.props.tech}
-                </div>
+                    <div className="">
+                        {this.props.tech}
+                    </div>
 
-            </section>
+                    <div className="">
+                        number of days {this.props.numbeOfDays}
+
+                    </div>
+
+
+                </section>
+
+                <hr />
+            </>
         )
     }
 }
