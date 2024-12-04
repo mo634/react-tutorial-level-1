7973c9325e48bca9425e878d3007a6a11b8ecf43
ArabDeveloper <arabdevelopers731@gmail.com>
2024-12-04 08:11:39 +0200

    17.states part 1

--- ComponentExample/my-app/src/components/Greeting.jsx
+++ ComponentExample/my-app/src/components/Greeting.jsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 import PropType from 'prop-types'
 
 export default class Greeting extends Component {
+
+    // create state 
+    state = {
+        // define your own data 
+        nameUpperCase: this.props.name.toUpperCase()
+    }
     static defaultProps = {
         name: 'unknown'
     }
@@ -13,8 +19,11 @@ export default class Greeting extends Component {
     render() {
         return (
             <div className="">
-                hello, your name is {this.props.name.toUpperCase()}
-                and your age is {this.props.age + 3} {/* 233 */}
+                hello, your name is {this.state.nameUpperCase}
+
+                <p>your first letter is {this.state.nameUpperCase[0]}</p>{/*mohamed -> MOHADED -> M*/}
+
+                and your age is {(this.props.age)} {/* 233 */}
             </div>
         )
     }
